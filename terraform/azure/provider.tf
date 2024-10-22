b8256d7abe0ccaf35f7b92bba87501bc45f64516
Kledson Basso <kledsonhugo@gmail.com>
2024-10-22 16:37:52 -0300

    Adicionar configuração do Terraform para Azure com grupos de recursos, redes virtuais e sub-redes

---
+++ terraform/azure/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.6.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-staticsitelbpeeringtf"
+    storage_account_name = "staticsitelbpeeringtf"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
