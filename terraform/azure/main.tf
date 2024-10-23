19fa04076d318c3d9ee3228bdc3a784bb8850890
Kledson Basso <kledsonhugo@gmail.com>
2024-10-23 10:51:55 -0300

    Adicionar grupo de segurança de rede nsgvnet20 com regras de segurança e associação à sub-rede privada no Terraform para Azure

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_security_group" "nsgvnet10" {
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     security_rule {
-        name                       = "HTTP"
+        name                       = "Inbound-All-HTTP"
         priority                   = 1001
         direction                  = "Inbound"
         access                     = "Allow"
@@ -61,7 +61,7 @@ resource "azurerm_network_security_group" "nsgvnet10" {
         destination_address_prefix = "*"
     }
     security_rule {
-        name                       = "FTP"
+        name                       = "Inbound-All-FTP"
         priority                   = 1011
         direction                  = "Inbound"
         access                     = "Allow"
@@ -73,40 +73,29 @@ resource "azurerm_network_security_group" "nsgvnet10" {
     }
 }
 
-# resource "azurerm_network_security_group" "nsgvnet20" {
-#     name                = "nsgvnet20"
-#     location            = azurerm_resource_group.rg.location
-#     resource_group_name = azurerm_resource_group.rg.name
-#     security_rule {
-#         name                       = "HTTP"
-#         priority                   = 1001
-#         direction                  = "Inbound"
-#         access                     = "Allow"
-#         protocol                   = "Tcp"
-#         source_port_range          = "*"
-#         destination_port_range     = "80"
-#         source_address_prefix      = "10.0.0.0/16"
-#         destination_address_prefix = "*"
-#     }
-#     security_rule {
-#         name                       = "FTP"
-#         priority                   = 1011
-#         direction                  = "Inbound"
-#         access                     = "Allow"
-#         protocol                   = "Tcp"
-#         source_port_range          = "*"
-#         destination_port_range     = "22"
-#         source_address_prefix      = "10.0.0.0/16"
-#         destination_address_prefix = "*"
-#     }
-# }
+resource "azurerm_network_security_group" "nsgvnet20" {
+    name                = "nsgvnet20"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    security_rule {
+        name                       = "Inbound-Internet-All"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Deny"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "Internet"
+        destination_address_prefix = "*"
+    }
+}
 
 resource "azurerm_subnet_network_security_group_association" "nsgsnvnet10pub" {
     subnet_id                 = azurerm_subnet.snvnet10pub.id
     network_security_group_id = azurerm_network_security_group.nsgvnet10.id
 }
 
-# resource "azurerm_subnet_network_security_group_association" "nsgsnvnet20priv" {
-#     subnet_id                 = azurerm_subnet.snvnet20priv.id
-#     network_security_group_id = azurerm_network_security_group.nsgvnet20.id
-# }
+resource "azurerm_subnet_network_security_group_association" "snvnet20priv_nsg" {
+    subnet_id                 = azurerm_subnet.snvnet20priv.id
+    network_security_group_id = azurerm_network_security_group.nsg_vnet20.id
+}
