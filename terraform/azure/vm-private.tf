29bc781f9b9f1971ab2ad0e1edaf08f42d6ad0b2
Kledson Basso <kledsonhugo@gmail.com>
2024-10-29 14:56:24 -0300

    Adicionar configuração de máquina virtual privada e atualizar recursos públicos em vm-public.tf

--- terraform/azure/vm-private.tf
+++ terraform/azure/vm-private.tf
@@ -1,39 +1,30 @@
-resource "azurerm_availability_set" "as_public" {
-    name                = "as-public"
+resource "azurerm_availability_set" "as_private" {
+    name                = "as-private"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-data "template_file" "cloud_init_public" {
+data "template_file" "cloud_init_private" {
     template = "${file("./scripts/cloud_init.sh")}"
 }
 
-resource "azurerm_public_ip" "vm01_pip_public" {
-    name                = "vm01-pip-public"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method   = "Static"
-    domain_name_label   = "vm01-pip-public"
-}
-
-resource "azurerm_network_interface" "vm01_nic_public" {
-    name                = "vm01-nic-public"
+resource "azurerm_network_interface" "vm02_nic_private" {
+    name                = "vm02-nic-private"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     ip_configuration {
-        name                          = "vm01-ipconfig-public"
-        subnet_id                     = azurerm_subnet.snvnet10pub.id
+        name                          = "vm02-ipconfig-private"
+        subnet_id                     = azurerm_subnet.snvnet20priv.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.vm01-pip.id
     }
 }
 
-resource "azurerm_virtual_machine" "vm01_public" {
-    name                             = "vm01-public"
+resource "azurerm_virtual_machine" "vm02_private" {
+    name                             = "vm02-private"
     location                         = azurerm_resource_group.rg.location
     resource_group_name              = azurerm_resource_group.rg.name
-    network_interface_ids            = [azurerm_network_interface.vm01_nic_public.id]
-    availability_set_id              = azurerm_availability_set.as_public.id
+    network_interface_ids            = [azurerm_network_interface.vm02_nic_private.id]
+    availability_set_id              = azurerm_availability_set.as_private.id
     vm_size                          = "Standard_D2s_v3"
     delete_os_disk_on_termination    = true
     delete_data_disks_on_termination = true
@@ -53,7 +44,7 @@ resource "azurerm_virtual_machine" "vm01_public" {
         computer_name  = "vm01"
         admin_username = "vmuser"
         admin_password = "Password1234!"
-        custom_data    = "${base64encode(data.template_file.cloud_init_public.rendered)}"
+        custom_data    = "${base64encode(data.template_file.cloud_init_private.rendered)}"
     }
     os_profile_linux_config {
         disable_password_authentication = false
