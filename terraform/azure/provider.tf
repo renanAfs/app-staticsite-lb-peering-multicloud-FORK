88ed4732dd46cf9bd4b2e9e925253ad9dee455b9
Kledson Basso <kledsonhugo@gmail.com>
2024-10-29 14:21:47 -0300

    Atualizar nome do grupo de recursos no arquivo provider.tf

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsitelbpeering-tf"
+    resource_group_name  = "rg-staticsitelbpeeringtf"
     storage_account_name = "staticsitelbpeeringkb"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
