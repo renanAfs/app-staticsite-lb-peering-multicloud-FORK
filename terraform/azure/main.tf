2f83328ea5f2c2ebdc82a46b2a2c1f05f26c24ea
Kledson Basso <kledsonhugo@gmail.com>
2024-10-29 14:28:05 -0300

    Remover configuração de máquina virtual e recursos associados do arquivo main.tf; adicionar novo arquivo vm.tf com a configuração da máquina virtual

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -94,71 +94,11 @@ resource "azurerm_network_security_group" "nsgvnet20" {
 resource "azurerm_subnet_network_security_group_association" "nsgsnvnet10pub" {
     subnet_id                 = azurerm_subnet.snvnet10pub.id
     network_security_group_id = azurerm_network_security_group.nsgvnet10.id
+    depends_on                = [ azurerm_subnet.snvnet10pub ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsgsnvnet20priv" {
     subnet_id                 = azurerm_subnet.snvnet20priv.id
     network_security_group_id = azurerm_network_security_group.nsgvnet20.id
-}
-
-resource "azurerm_availability_set" "as" {
-    name                = "as"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-}
-
-data "template_file" "cloud_init" {
-    template = "${file("./scripts/cloud_init.sh")}"
-}
-
-resource "azurerm_public_ip" "vm01-pip" {
-    name                = "vm01-pip"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method   = "Static"
-    domain_name_label   = "vm01-pip"
-}
-
-resource "azurerm_network_interface" "vm01-nic" {
-    name                = "vm01-nic"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    ip_configuration {
-        name                          = "vm01-ipconfig"
-        subnet_id                     = azurerm_subnet.snvnet10pub.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.vm01-pip.id
-    }
-}
-
-resource "azurerm_virtual_machine" "vm01" {
-    name                             = "vm01"
-    location                         = azurerm_resource_group.rg.location
-    resource_group_name              = azurerm_resource_group.rg.name
-    network_interface_ids            = [azurerm_network_interface.vm01-nic.id]
-    availability_set_id              = azurerm_availability_set.as.id
-    vm_size                          = "D2as_v4"
-    delete_os_disk_on_termination    = true
-    delete_data_disks_on_termination = true
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "0001-com-ubuntu-server-jammy"
-        sku       = "22_04-lts"
-        version   = "latest"
-    }
-    storage_os_disk {
-        name              = "vm01-os-disk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
-    os_profile {
-        computer_name  = "vm01"
-        admin_username = "vmuser"
-        admin_password = "Password1234!"
-        custom_data    = "${base64encode(data.template_file.cloud_init.rendered)}"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
+    depends_on                = [ azurerm_subnet.snvnet20priv ]
 }
