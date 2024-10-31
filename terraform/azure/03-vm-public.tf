2058e700377af4631b244d2b501ae8d5522c62e2
Kledson Basso <kledsonhugo@gmail.com>
2024-10-31 12:50:58 -0300

    Descomentar referência de imagem de armazenamento para a VM pública no arquivo 03-vm-public.tf

--- terraform/azure/03-vm-public.tf
+++ terraform/azure/03-vm-public.tf
@@ -33,17 +33,17 @@ resource "azurerm_virtual_machine" "vm01_public" {
     vm_size                          = "Standard_D2s_v3"
     delete_os_disk_on_termination    = true
     # delete_data_disks_on_termination = true
-    # storage_image_reference {
-    #     publisher = "Canonical"
-    #     offer     = "0001-com-ubuntu-server-jammy"
-    #     sku       = "22_04-lts"
-    #     version   = "latest"
-    # }
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "0001-com-ubuntu-server-jammy"
+        sku       = "22_04-lts"
+        version   = "latest"
+    }
     storage_os_disk {
         name              = "vm01-os-disk-public"
         # caching           = "ReadWrite"
         create_option     = "FromImage"
-        image_uri         = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
+        # image_uri         = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
         managed_disk_type = "Standard_LRS"
     }
     os_profile {
