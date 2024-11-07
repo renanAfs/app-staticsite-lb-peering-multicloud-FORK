788760344cee578e0d3352617b876b33f881ab9e
renanAfs <rm93744@fiap.com.br>
2024-11-07 19:28:51 -0300

    Trigger Azure

--- terraform/azure/modules/compute/compute.tf
+++ terraform/azure/modules/compute/compute.tf
@@ -154,49 +154,49 @@ resource "azurerm_virtual_machine" "vm02_public" {
 
 
 
-#vm privada
+# #vm privada
 
 
-resource "azurerm_network_interface" "vm02_nic_private" {
-    name                = "vm02-nic-private"
-    location            = var.rglocation
-  resource_group_name = var.rgname
-    ip_configuration {
-        name                          = "vm02-ipconfig-private"
-        subnet_id                     = var.snvnet20priv
-        private_ip_address_allocation = "Dynamic"
-    }
-}
+# resource "azurerm_network_interface" "vm02_nic_private" {
+#     name                = "vm02-nic-private"
+#     location            = var.rglocation
+#   resource_group_name = var.rgname
+#     ip_configuration {
+#         name                          = "vm02-ipconfig-private"
+#         subnet_id                     = var.snvnet20priv
+#         private_ip_address_allocation = "Dynamic"
+#     }
+# }
 
 
 
-resource "azurerm_virtual_machine" "vm02_private" {
-    name                          = "vm02-private"
-    location            = var.rglocation
-  resource_group_name = var.rgname
-    network_interface_ids         = [azurerm_network_interface.vm02_nic_private.id]
-    vm_size                       = "Standard_D2s_v3"
-    delete_os_disk_on_termination = true
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "0001-com-ubuntu-server-jammy"
-        sku       = "22_04-lts"
-        version   = "latest"
-    }
-    storage_os_disk {
-        name              = "vm02-os-disk-private"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
-    os_profile {
-        computer_name  = "vm02-private"
-        admin_username = "azureuser"
-        admin_password = "Password1234!"
-    }
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-}
+# resource "azurerm_virtual_machine" "vm02_private" {
+#     name                          = "vm02-private"
+#     location            = var.rglocation
+#   resource_group_name = var.rgname
+#     network_interface_ids         = [azurerm_network_interface.vm02_nic_private.id]
+#     vm_size                       = "Standard_D2s_v3"
+#     delete_os_disk_on_termination = true
+#     storage_image_reference {
+#         publisher = "Canonical"
+#         offer     = "0001-com-ubuntu-server-jammy"
+#         sku       = "22_04-lts"
+#         version   = "latest"
+#     }
+#     storage_os_disk {
+#         name              = "vm02-os-disk-private"
+#         create_option     = "FromImage"
+#         managed_disk_type = "Standard_LRS"
+#     }
+#     os_profile {
+#         computer_name  = "vm02-private"
+#         admin_username = "azureuser"
+#         admin_password = "Password1234!"
+#     }
+#     os_profile_linux_config {
+#         disable_password_authentication = false
+#     }
+# }
 
 
 
