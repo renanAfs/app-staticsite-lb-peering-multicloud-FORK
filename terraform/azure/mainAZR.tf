0cfd0c1ff2933098d93ab78344956009f6de56db
renan <rm93744@fiap.com.br>
2024-11-07 00:31:10 -0300

    Trigger Pipeline Azure

--- terraform/azure/mainAZR.tf
+++ terraform/azure/mainAZR.tf
@@ -5,7 +5,7 @@ module "network" {
 module "compute" {
  source = "./modules/compute"
   rgname = module.network.rgname
-  rglocation = module.network.location
+  rglocation = module.network.rglocation
   vnet10 = module.network.vnet10
   vnet20 = module.network.vnet20
   snvnet10pub1a = module.network.snvnet10pub1a
